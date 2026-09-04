9ff154a4b6d745ff5b29177c7924a464e60e1d3f
neel-252246 <neel2005sinha@gmail.com>
2026-09-05 03:20:21 +0530

    Create PinValidator.java

---
+++ src/main/java/StringManipulation_AssignmentProblems/PinValidator.java
@@ -0,0 +1,16 @@
+﻿package StringManipulation_AssignmentProblems;
+
+public class PinValidator {
+    public static void checkPinLength(String pin) {
+        if (pin.length() != 4) {
+            System.out.println("Invalid PIN \u2014 must be exactly 4 digits.");
+        } else {
+            System.out.println("PIN length OK.");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkPinLength("482");
+        checkPinLength("4820");
+    }
+}
